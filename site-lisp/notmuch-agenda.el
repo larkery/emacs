6e393deb7344ec683810d50c4400f768d84d911d
Tom Hinton <t@larkery.com>
2018-05-02 20:50:40 +0100

    add agenda render

--- site-lisp/notmuch-agenda.el
+++ site-lisp/notmuch-agenda.el
@@ -72,6 +72,109 @@
           (format "<%s %s-%s>" start-d start-t end-t)
         (format "<%s %s>--<%s %s>" start-d start-t end-d end-t))))
 
+(defun notmuch-agenda-insert-agenda (event zone-map)
+  (require 'org)
+  (let* ((dtstart (notmuch-agenda-event-time event zone-map 'DTSTART))
+         (wins (current-window-configuration))
+         (org-agenda-sticky nil)
+         (inhibit-redisplay t)
+         (year (nth 5 dtstart))
+         (month (nth 4 dtstart))
+         (day (nth 3 dtstart))
+         
+         (org-agenda-custom-commands '(("q" "Mail agenda" ((agenda ""))))))
+    (org-eval-in-environment
+     (org-make-parameter-alist
+      `(org-agenda-span 'day
+        org-agenda-start-day ,(format "%04d-%02d-%02d" year month day)
+        org-agenda-use-time-grid nil
+        org-agenda-remove-tags t
+        org-agenda-window-setup 'nope))
+
+     (progn
+       (save-excursion
+         (org-agenda nil "q")
+         (org-agenda-redo)
+         (setq org-agenda-mail-buffer (current-buffer)))
+       (set-window-configuration wins)
+       (let ((p (point))
+             pa)
+         ;; copy text
+         (insert-buffer-substring org-agenda-mail-buffer)
+
+         ;; copy markers
+         (save-restriction
+           (narrow-to-region p (point))
+           (let ((org-marker-regions
+                  (with-current-buffer
+                      org-agenda-mail-buffer
+                    (setq pa (point-min))
+                    (gnus-find-text-property-region (point-min) (point-max) 'org-marker))))
+             (loop for marker in org-marker-regions
+                   do
+                   (add-text-properties
+                    (+ p (- (car marker) pa)) (+ p (- (cadr marker) pa))
+                    `(org-marker
+                      ,(copy-marker (get-text-property (car marker) 'org-marker org-agenda-mail-buffer))))
+
+                   (set-marker (car marker) nil)
+                   (set-marker (cadr marker) nil))))
+
+         ;; copy faces via font-lock-face
+         (save-restriction
+           (narrow-to-region p (point))
+           (let ((face-regions (gnus-find-text-property-region (point-min) (point-max) 'face)))
+             (loop for range in face-regions
+                   do
+                   (let ((face (get-text-property (car range) 'face)))
+                     (add-text-properties
+                      (car range) (cadr range)
+                      `(font-lock-face ,face)))
+
+
+                   (set-marker (car range) nil)
+                   (set-marker (cadr range) nil))))
+
+         (kill-buffer org-agenda-mail-buffer)
+         (put-text-property p (point) 'keymap
+                            org-agenda-keymap)))
+                             )
+    )
+  )
+
+(defun notmuch-agenda-insert-summary (event zone-map)
+  (let* ((summary (icalendar--get-event-property event 'SUMMARY))
+         (comment (icalendar--get-event-property event 'COMMENT))
+         (location (icalendar--get-event-property event 'LOCATION))
+         (organizer (icalendar--get-event-property event 'ORGANIZER))
+         (attendees (icalendar--get-event-properties event 'ATTENDEE))
+         (summary (when summary (icalendar--convert-string-for-import summary)))
+         (comment (when comment (icalendar--convert-string-for-import comment)))
+
+         (dtstart (notmuch-agenda-event-time event zone-map 'DTSTART))
+         (dtend (notmuch-agenda-event-time event zone-map 'DTEND))
+         (rrule (icalendar--get-event-property event 'RRULE))
+         (rdate (icalendar--get-event-property event 'RDATE))
+         (duration (icalendar--get-event-property event 'DURATION))
+         
+         
+         (friendly-date (notmuch-agenda-friendly-date dtstart dtend rrule rdate duration))
+         )
+
+    (when friendly-date (insert friendly-date "\n"))
+    (when summary (insert "Summary: "summary"\n"))
+    (when comment (insert "Comment: "comment"\n"))
+    (when location (insert "Location: "location"\n"))
+    (when organizer (insert "Organizer: "organizer"\n"))
+    (when attendees (insert "Attending: \n"))
+    (while attendees
+      (insert "  - " (car attendees) "\n")
+      (setq attendees (cdr attendees)))
+
+    ;; also insert org-mode agenda summary for this day
+
+    ))
+
 (defun notmuch-agenda-insert-part (msg part content-type nth depth button)
   (let (icalendar-element)
     (with-temp-buffer
@@ -95,33 +198,10 @@
              (zone-map (icalendar--convert-all-timezones events)))
         (dolist (event events)
           ;; insert event description string
-          (let* ((summary (icalendar--get-event-property event 'SUMMARY))
-                 (comment (icalendar--get-event-property event 'COMMENT))
-                 (location (icalendar--get-event-property event 'LOCATION))
-                 (organizer (icalendar--get-event-property event 'ORGANIZER))
-                 (attendees (icalendar--get-event-properties event 'ATTENDEE))
-                 (summary (when summary (icalendar--convert-string-for-import summary)))
-                 (comment (when comment (icalendar--convert-string-for-import comment)))
-
-                 (dtstart (notmuch-agenda-event-time event zone-map 'DTSTART))
-                 (dtend (notmuch-agenda-event-time event zone-map 'DTEND))
-                 (rrule (icalendar--get-event-property event 'RRULE))
-                 (rdate (icalendar--get-event-property event 'RDATE))
-                 (duration (icalendar--get-event-property event 'DURATION))
-                 
-                 
-                 (friendly-date (notmuch-agenda-friendly-date dtstart dtend rrule rdate duration))
-                 )
-
-            (when friendly-date (insert friendly-date "\n"))
-            (when summary (insert "Summary: "summary"\n"))
-            (when comment (insert "Comment: "comment"\n"))
-            (when location (insert "Location: "location"\n"))
-            (when organizer (insert "Organizer: "organizer"\n"))
-            (when attendees (insert "Attending: \n"))
-            (while attendees
-              (insert "  - " (car attendees) "\n")
-              (setq attendees (cdr attendees)))))
+          (notmuch-agenda-insert-summary event zone-map)
+          (notmuch-agenda-insert-agenda event zone-map))
+        
+        
         t))))
 
 
