36cce7954ab2827408427ce7eb5ddca2e5719ece
Tom Hinton <tom.hinton@cse.org.uk>
2018-08-29 17:25:36 +0100

    fix dired faces

--- site-lisp/themes/tweaks-theme.el
+++ site-lisp/themes/tweaks-theme.el
@@ -4,13 +4,14 @@
  'tweaks
  '(org-todo ((default (:weight bold))))
  '(dired-subtree-depth-1-face ((t (:slant italic))))
- '(dired-subtree-depth-2-face ((t (:slant nil))))
- '(dired-subtree-depth-3-face ((t)))
- '(dired-subtree-depth-4-face ((t)))
- '(dired-subtree-depth-5-face ((t)))
- '(dired-subtree-depth-6-face ((t)))
+ '(dired-subtree-depth-2-face ((t (:slant normal))))
+ '(dired-subtree-depth-3-face ((t (:slant italic))))
+ '(dired-subtree-depth-4-face ((t (:slant normal))))
+ '(dired-subtree-depth-5-face ((t (:slant italic))))
+ '(dired-subtree-depth-6-face ((t (:slant normal))))
 
  '(notmuch-search-unread-face ((t (:foreground "white" :distant-foreground "black" :weight bold))))
+ '(symbol-overlay-default-face ((t (:underline "grey50"))))
  )
 
 (provide-theme 'tweaks)
