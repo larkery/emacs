e6a426234dd83cdf75afcdfd67b2bfcca7ba95af
Tom Hinton <tom.hinton@cse.org.uk>
2019-02-08 17:41:25 +0000

    click mode line

--- startup/mode-line.el
+++ startup/mode-line.el
@@ -15,17 +15,19 @@
              (projectile-project-p))
     (let ((root (projectile-project-root)))
       (when (string-prefix-p root (buffer-file-name))
-        (concat " (" (propertize (projectile-project-name)
-                                 'face 'italic
-                                 ) ")")))))
+        (concat " ("
+                (propertize (projectile-project-name)
+                            'face 'underline
+                            'mouse-face 'mode-line-highlight
+                            'local-map (make-mode-line-mouse-map 'mouse-1 'projectile-dired-other-window))
+                ")")))))
 
 (defun mode-line-pad-right (rhs)
   "Return empty space using FACE and leaving RESERVE space on the right."
   (let* ((the-rhs (format-mode-line rhs))
          (reserve (length the-rhs)))
     (list
-     (propertize " " 'display `((space :align-to (- right ,reserve)))) rhs)
-    ))
+     (propertize " " 'display `((space :align-to (- right ,reserve)))) rhs)))
 
 (setq-default
  frame-title-format
@@ -34,14 +36,16 @@
                    (buffer-file-name
                     (:eval
                     (if (projectile-project-p)
-                        (list "["(projectile-project-name)"]"
-                              " %b")
+                        (list "[" (projectile-project-name) "]" " %b")
                       buffer-file-name))
                     "%b"))
  
  mode-line-format
- '("%5l "
-   "%* %b"
+ `("%5l "
+   "%* "
+   ,(propertize "%b"
+               'mouse-face 'mode-line-highlight
+               'local-map (make-mode-line-mouse-map 'mouse-1 'dired-here-please))
    (:eval (mode-line-get-project))
    (:eval (mode-line-get-host))
    (:eval (mode-line-pad-right
