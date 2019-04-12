bbd7ba2d001ae96ea2f4fbbdfce6a51195324826
Tom Hinton <tom.hinton@cse.org.uk>
2019-04-12 16:20:33 +0100

    make org have icons

--- startup/org.el
+++ startup/org.el
@@ -18,6 +18,11 @@
   (org-id-locations-file "~/notes/.metadata/org-id-locations")
   (org-agenda-insert-diary-extract-time t)
   (org-adapt-indentation nil)
+  (org-agenda-span 14)
+  (org-agenda-category-icon-alist
+   `(("calendar" (display ,(all-the-icons-faicon "calendar" :height 0.75)))
+     ("birthday" (display ,(all-the-icons-faicon "birthday-cake" :height 0.75)))
+     ("holiday" (display ,(all-the-icons-faicon "calendar-o" :height 0.75)))))
   
   (org-babel-load-languages '((emacs-lisp . t) (dot . t) (ditaa . t)))
 
@@ -121,3 +126,68 @@
   (advice-add 'org-agenda :around 'org-agenda-date-at-point)
   
   )
+
+
+;; UK public holidays, and other UK notable dates.
+(setq calendar-holidays
+      '((holiday-fixed 1 1 "New Year's Day")
+        (holiday-new-year-bank-holiday)
+        (holiday-fixed 2 14 "Valentine's Day")
+        (holiday-fixed 3 17 "St. Patrick's Day")
+        (holiday-fixed 4 1 "April Fools' Day")
+        (holiday-easter-etc -47 "Shrove Tuesday")
+        (holiday-easter-etc -21 "Mother's Day")
+        (holiday-easter-etc -2 "Good Friday")
+        (holiday-easter-etc 0 "Easter Sunday")
+        (holiday-easter-etc 1 "Easter Monday")
+        (holiday-float 5 1 1 "Early May Bank Holiday")
+        (holiday-float 5 1 -1 "Spring Bank Holiday")
+        (holiday-float 6 0 3 "Father's Day")
+        (holiday-float 8 1 -1 "Summer Bank Holiday")
+        (holiday-fixed 10 31 "Halloween")
+        (holiday-fixed 12 24 "Christmas Eve")
+        (holiday-fixed 12 25 "Christmas Day")
+        (holiday-fixed 12 26 "Boxing Day")
+        (holiday-christmas-bank-holidays)
+        (holiday-fixed 12 31 "New Year's Eve")))
+;; N.B. It is assumed that 1 January is defined with holiday-fixed -
+;; this function only returns any extra bank holiday that is allocated
+;; (if any) to compensate for New Year's Day falling on a weekend.
+;;
+;; Where 1 January falls on a weekend, the following Monday is a bank
+;; holiday.
+(defun holiday-new-year-bank-holiday ()
+  (let ((m displayed-month)
+        (y displayed-year))
+    (calendar-increment-month m y 1)
+    (when (<= m 3)
+      (let ((d (calendar-day-of-week (list 1 1 y))))
+        (cond ((= d 6)
+                (list (list (list 1 3 y)
+                            "New Year's Day Bank Holiday")))
+              ((= d 0)
+                (list (list (list 1 2 y)
+                            "New Year's Day Bank Holiday"))))))))
+
+;; N.B. It is assumed that 25th and 26th are defined with holiday-fixed -
+;; this function only returns any extra bank holiday(s) that are
+;; allocated (if any) to compensate for Christmas Day and/or Boxing Day
+;; falling on a weekend.
+(defun holiday-christmas-bank-holidays ()
+  (let ((m displayed-month)
+        (y displayed-year))
+    (calendar-increment-month m y -1)
+    (when (>= m 10)
+      (let ((d (calendar-day-of-week (list 12 25 y))))
+        (cond ((= d 5)
+                (list (list (list 12 28 y)
+                            "Boxing Day Bank Holiday")))
+              ((= d 6)
+                (list (list (list 12 27 y)
+                            "Boxing Day Bank Holiday")
+                      (list (list 12 28 y)
+                            "Christmas Day Bank Holiday")))
+              ((= d 0)
+                (list (list (list 12 27 y)
+                            "Christmas Day Bank Holiday"))))))))
+
