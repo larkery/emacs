88de47526f92699be3e169e920b57f8d0958fd70
Tom Hinton <tom.hinton@cse.org.uk>
2020-02-17 12:43:20 +0000

    make hydra better

--- site-lisp/org-agenda-event-hydra.el
+++ site-lisp/org-agenda-event-hydra.el
@@ -6,47 +6,107 @@
 (defvar org-agenda-event-hydra-location nil)
 (defvar org-agenda-event-hydra-summary nil)
 (defvar org-agenda-event-hydra-all-day nil)
+(defvar org-agenda-event-hydra-target nil)
 
 (defhydra org-agenda-event-hydra
   (:hint nil)
   "
-_d_ate: %s(format-time-string (if org-agenda-event-hydra-all-day \"%a %d %b %Y\" \"%a %d %b %Y %H:%M\") org-agenda-event-hydra-date) _s_: %s`org-agenda-event-hydra-summary
+_s_: %s`org-agenda-event-hydra-summary, _l_oc: %s`org-agenda-event-hydra-location
+_d_ate: %s(format-time-string \"%a %d %b %Y\" org-agenda-event-hydra-date) _t_ime: %s(if org-agenda-event-hydra-all-day \"All day\" (format-time-string \"%H:%M\" org-agenda-event-hydra-date))
+_c_al: %s`org-agenda-event-hydra-target
 "
   ("d" (setq org-agenda-event-hydra-date (org-read-date t t nil "Start date: " nil)))
-  ("D" (setq org-agenda-event-hydra-all-day (not org-agenda-event-hydra-all-day)))
+  ("t" (if org-agenda-event-hydra-all-day
+           (let* ((new-time
+                   (read-string "Start time: "))
+                  
+                  (new-time (parse-time-string new-time))
+                  (min (nth 1 new-time))
+                  (hr (nth 2 new-time)))
+             (when (and min hr)
+               (setq
+                org-agenda-event-hydra-all-day nil
+                org-agenda-event-hydra-date
+                (apply
+                 #'encode-time
+                 0 min hr
+                 (nthcdr 3 (decode-time org-agenda-event-hydra-date))))))
+         (setq org-agenda-event-hydra-all-day t)))
+  
   ("l" (setq org-agenda-event-hydra-location
              (read-string "Location: " org-agenda-event-hydra-location)))
   ("s" (setq org-agenda-event-hydra-summary
              (read-string "Summary: " org-agenda-event-hydra-summary)))
-  ("t" (setq org-agenda-event-hydra-target
-             (completing-read "Target: " (org-agenda-files) nil t org-agenda-event-hydra-target)))
-
-  ("RET" org-agenda-event-hydra-finish)
-  )
+  ("c" (let ((oaf (org-agenda-files)))
+         (setq org-agenda-event-hydra-target
+               (or
+                (cadr (member org-agenda-event-hydra-target oaf))
+                (car oaf)))))
+  ("RET" org-agenda-event-hydra-finish :exit t))
 
 (defun org-agenda-event-hydra-finish ()
   (interactive)
 
-  (let ((org-capture-templates
-         `(("i" "Insert this thing" entry
-            (file ,org-agenda-event-hydra-target)
-            ,(s-lex-format "* ${org-agenda-event-hydra-summary}
+  (let* ((location
+          (if org-agenda-event-hydra-location
+              (format ":LOCATION: %s" org-agenda-event-hydra-location)
+            ""))
+
+         (formatted-date
+          (format-time-string (if org-agenda-event-hydra-all-day
+                                  "%Y-%m-%d %a"
+                                "%Y-%m-%d %a %H:%M")
+                    org-agenda-event-hydra-date))
+         
+         (org-capture-templates
+          `(("i" "Insert this thing" entry
+             (file ,org-agenda-event-hydra-target)
+             ,(s-lex-format "* ${org-agenda-event-hydra-summary}
 :PROPERTIES:
-:END:"
-                     org-agenda-event-hydra-summary
-                     
-                     )
-            ))
-         )))
-  )
+${location}
+:END:
+<${formatted-date}>
+"
+                            )))))
+    (org-capture nil "i")
+    ))
 
 (defun org-agenda-event-hydra-start (summary)
   (interactive "sSummary: ")
 
-  (setq org-agenda-event-hydra-summary summary
-        org-agenda-event-hydra-location nil
-        org-agenda-event-hydra-date (org-get-cursor-date t)
-        org-agenda-event-hydra-target (car (org-agenda-files)))
+  (let* ((summary "12:50 March for health @ wills building")
+         (match (string-match
+                (rx bos (? (group digit digit) (? ":" (group digit digit)) " ")
+                    (group (*? (not (any "@"))))
+                    (? (? " ") "@" (? " ") (group (* anything)))
+                    eos)
+                summary
+                )))
+    (match-string 3 summary))
+  
+  (let* ((match (string-match
+                 (rx bos (? (group digit digit) (? ":" (group digit digit)) " ")
+                     (group (*? (not (any "@"))))
+                     (? (? " ") "@" (? " ") (group (* anything)))
+                     eos)
+                 summary))
+         (date (org-get-cursor-date t))
+         (hh (when (match-string 1 summary)
+               (string-to-number (match-string 1 summary))))
+         (mm (when (match-string 2 summary)
+               (string-to-number (match-string 2 summary))))
+         (date
+          (if (or hh mm)
+              (apply
+               #'encode-time
+               0 (or mm 0) (or hh 0)
+               (nthcdr 3 (decode-time date)))
+            date)))
+    
+    (setq org-agenda-event-hydra-summary (match-string 3 summary)
+          org-agenda-event-hydra-location (match-string 4 summary)
+          org-agenda-event-hydra-date date
+          org-agenda-event-hydra-target (car (org-agenda-files))))
   
   (org-agenda-event-hydra/body))
 
