3a8b5a6458df6f2b43057646edf6a81014dcf4ce
Tom Hinton <tom.hinton@cse.org.uk>
2020-11-22 10:54:52 +0000

    Add more tabulated-list-mode functions

--- site-lisp/tabulated-list-utils.el
+++ site-lisp/tabulated-list-utils.el
@@ -1,7 +1,5 @@
 (require 'tabulated-list)
 
-
-
 (defun tabulated-list-put-tag-all (tag)
   (save-excursion
     (goto-char (point-min))
@@ -17,5 +15,24 @@
           (while (not (eobp))
             (tabulated-list-put-tag tag t))))
     (tabulated-list-put-tag tag next-line)))
+
+(defun tabulated-list-marked-or-current-id (mark)
+  (let (out)
+    (save-excursion
+      (goto-char (point-min))
+      (while (not (eobp))
+        (when (= mark (char-after))
+          (push (tabulated-list-get-id) out))
+        (forward-line)))
+    (unless out
+      (push (tabulated-list-get-id) out))
+    out))
+
+(defun tabulated-list-goto-id (id)
+  (goto-char (point-min))
+  (while (not (or (eobp)
+                  (string= (tabulated-list-get-id) id)))
+    (forward-line))
+  (string= (tabulated-list-get-id) id))
 
 (provide 'tabulated-list-utils)
